c2a525b9446c6f1a60794096b5c95d119e10f1ca
HyeonjinChoi <63872787+HyeonjinChoi@users.noreply.github.com>
2024-06-06 23:43:02 +0900

    [level 1] Title: 과일 장수, Time: 0.35 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -1,15 +1,10 @@
 import java.util.*;
-import java.util.stream.*;
 
 class Solution {
     public int solution(int k, int m, int[] score) {
-        List<Integer> list = Arrays.stream(score).boxed().sorted().collect(Collectors.toList());
-        
-        int size = score.length, pivot = 0, sum = 0;
-        for (int i = size % m; i < size; i++) {
-            if (i % m == size % m) pivot = list.get(i);
-            sum += pivot;
-        }
+        Arrays.sort(score);
+        int sum = 0, size = score.length;
+        for (int i = size - m; i >= 0; i -= m) sum += score[i] * m;
         
         return sum;
     }
