122b49126c01f664cdc8207ad114daa9d0c4af2e
HyeonjinChoi <63872787+HyeonjinChoi@users.noreply.github.com>
2024-06-29 17:46:35 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,18 +1,14 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int x = 0, y = 0;
         int total = brown + yellow;
         
         for (int i = 3; i < total; i++) {
             if (total % i == 0) {
                 int row = i, col = total / i;
-                if (2 * row + 2 * col - 4 == brown) {
-                    x = row;
-                    y = col;
-                }
+                if (2 * row + 2 * col - 4 == brown) return new int[]{col, row};
             }
         }
         
-        return new int[]{x, y};
+        return new int[]{0, 0};
     }
 }
